acb93efeac7e16de8efd73ed4270713c194b01e5
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-29 14:04:06 +0100

    add example

--- recipes/function/Recipe.php
+++ recipes/function/Recipe.php
@@ -2,7 +2,13 @@
 
 namespace Codger\Javascript;
 
+/**
+ * An example recipe for a javascript function.
+ */
 return function () : Func {
     $function = new Func;
+    $function->set('name', 'foo')
+        ->output('php://stdout');
+    return $function;
 };
 
