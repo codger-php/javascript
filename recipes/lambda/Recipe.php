35f48bf98d90ed2790f9d2e1d8a2d89e39a09019
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-29 14:04:13 +0100

    add example

--- recipes/lambda/Recipe.php
+++ recipes/lambda/Recipe.php
@@ -2,7 +2,18 @@
 
 namespace Codger\Javascript;
 
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
+/**
+ * An example recipe for a javascript lambda.
+ */
 return function () : Func {
     $function = new Func;
+    $twig = new Twig_Environment(new Twig_Loader_Filesystem(dirname(__DIR__).'/templates'));
+    $function->output('php://stdout')
+        ->addArgument(new Argument($twig, 'foo'))
+        ->setBody('return false;');
+    return $function;
 };
 
